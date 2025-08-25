76ed38e76fdedc39e151467c0847bb7be7acdddd
WenedyDutra <wenedyas@gmail.com>
2025-08-25 13:13:06 -0300

    Subindo Projeto

---
+++ Entities/Notificacoes/Notifica.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notificacoes
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+            notificacoes = new List<Notifica>();
+        }
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+        [NotMapped]
+        public string mensagem { get; set; }
+        [NotMapped]
+        public List<Notifica> notificacoes;
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if(string.IsNullOrEmpty(valor) || string.IsNullOrEmpty(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade,
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrEmpty(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade,
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Infra/Configuracao/ContextBase.cs
@@ -0,0 +1,44 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuracao
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiro {  get; set; }
+        public DbSet<SistemaFinanceiro> UsuarioSistemaFinanceiro { get; set; }
+        public DbSet<SistemaFinanceiro> Categoria { get; set; }
+        public DbSet<SistemaFinanceiro> Despesa{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+        public string ObterStringConexao()
+        {
+            return "Data Source=DESKTOP-9U3HU0O; Initial Catalog=Financeiro;Integrated Security=True";
+        }
+    }
+}
